3e052f4a3d5286606fbaeb4548dc8e6bea095de9
JyJumu <118355179+JyJumu@users.noreply.github.com>
2025-02-20 14:23:54 +0100

    Update and rename ucb.py to ucb1.py

--- ucb1.py
+++ ucb1.py
@@ -1,5 +1,5 @@
 """
-Module: algorithms/ucb.py
+Module: algorithms/ucb1.py
 Description: Implementación del algoritmo epsilon-greedy para el problema de los k-brazos.
 
 Author: Jesús Verdú Chacón & Jorge López Abad
@@ -16,9 +16,9 @@ import numpy as np
 
 from algorithms.algorithm import Algorithm
 
-class UCB(Algorithm):
+class UCB1(Algorithm):
 
-    def __init__(self, k: int, epsilon: float = 0.1):
+    def __init__(self, k: int, alfa: float = 0.1):
         """
         Inicializa el algoritmo UCB.
 
@@ -26,17 +26,20 @@ class UCB(Algorithm):
         :param epsilon: Probabilidad de exploración (seleccionar un brazo al azar).
         :raises ValueError: Si epsilon no está en [0, 1].
         """
-        assert 0 <= epsilon <= 1, "El parámetro epsilon debe estar entre 0 y 1."
+        assert 0 < alfa < 1, "El parámetro alfa debe estar entre 0 y 1."
 
         super().__init__(k)
-        self.epsilon = epsilon
+        self.alfa = alfa
+        self.uas: np.ndarray = np.zeros(k, dtype=float) 
 
     def select_arm(self) -> int:
         """
-        Selecciona un brazo basado en la política epsilon-greedy.
+        Selecciona un brazo basado en la política UCB1.
         :return: índice del brazo seleccionado.
         """
 
+        
+        
         if np.random.random() < self.epsilon:
             # Selecciona un brazo al azar
             chosen_arm = np.random.choice(self.k)
