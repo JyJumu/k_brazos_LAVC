15bf7421fb7c1b3c0e5c212a96c83102149de57d
JyJumu <118355179+JyJumu@users.noreply.github.com>
2025-02-20 13:54:37 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,20 @@
+"""
+Module: algorithms/__init__.py
+Description: Contiene las importaciones y modulos/clases públicas del paquete arms.
+
+Authors: Jesús Verdú Chacón & Jorge López Abad
+Email: jesus.v.c@um.es & jorge.lopeza@um.es
+Date: 2025/02/20
+
+This software is licensed under the GNU General Public License v3.0 (GPL-3.0),
+with the additional restriction that it may not be used for commercial purposes.
+
+For more details about GPL-3.0: https://www.gnu.org/licenses/gpl-3.0.html
+"""
+
+# Importación de módulos o clases
+from .algorithm import Algorithm
+from .epsilon_greedy import EpsilonGreedy
+
+# Lista de módulos o clases públicas
+__all__ = ['Algorithm', 'EpsilonGreedy']
