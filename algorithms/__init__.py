2fd571cccb71821cbb2ebbdf811f907078c1f7ac
JyJumu <118355179+JyJumu@users.noreply.github.com>
2025-02-24 20:53:44 +0100

    Update __init__.py

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -17,4 +17,4 @@ from .algorithm import Algorithm
 from .epsilon_greedy import EpsilonGreedy
 
 # Lista de módulos o clases públicas
-__all__ = ['Algorithm', 'EpsilonGreedy']
+__all__ = ['Algorithm', 'EpsilonGreedy', 'UCB1', 'UCB2']
