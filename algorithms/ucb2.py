a410d1c61dbc335d335292242a6d6a4e275d68a9
JyJumu <118355179+JyJumu@users.noreply.github.com>
2025-02-24 19:55:58 +0100

    Update and rename ucb2 to ucb2.py

--- algorithms/ucb2.py
+++ algorithms/ucb2.py
@@ -13,23 +13,28 @@ For more details about GPL-3.0: https://www.gnu.org/licenses/gpl-3.0.html
 """
 
 import numpy as np
+import math
 
 class UCB2(Algorithm):
 
-    def __init__(self, k: int, c: float = 0.1):
+    def __init__(self, k: int, alfa: float = 0.1):
         """
         Inicializa el algoritmo UCB.
 
         :param k: Número de brazos.
-        :param epsilon: Probabilidad de exploración (seleccionar un brazo al azar).
+        :param alfa: Parámetro de ajuste entre exploración y explotación.
         :raises ValueError: Si epsilon no está en [0, 1].
         """
-        assert 0 < c < 1, "El parámetro alfa debe estar entre 0 y 1."
+        assert 0 < alfa < 1, "El parámetro alfa debe estar entre 0 y 1."
 
         super().__init__(k)
-        self.c = c
+        self.alfa = alfa
         self.uas: np.ndarray = np.zeros(k, dtype=float)
         self.ucbs: np.ndarray = np.zeros(k, dtype=float)
+        self.kas: np.darray = np.zeros(k, dtype=int)
+
+    def tau(ka: int) -> int:
+        return math.ceil((1 + self.alfa)**ka)
 
     def select_arm(self, t: int) -> int:
         """
@@ -39,11 +44,20 @@ class UCB2(Algorithm):
         """
 
         for i in range(self.k):
-            self.uas[i] = np.sqrt(2 * np.log(t+1) / self.counts[i])
+            self.uas[i] = np.sqrt((1 + self.alfa) * np.log(math.e * t / self.tau(self.kas[i])) / 2 * self.tau(self.kas[i]))
         
         for i in range(self.k):
-            self.ucbs[i] = self.values[i] + self.c * self.uas[i]
+            self.ucbs[i] = self.values[i] + self.uas[i]
 
         chosen_arm = np.argmax(self.ucbs)
 
         return chosen_arm
+
+    def update(self, chosen_arm: int, reward: float):
+        num_veces = self.tau(self.kas[chosen_arm] + 1) - self.tau(self.kas[chosen_arm])
+        for i in range(num_veces)
+            super().update(chosen_arm, reward)
+
+        self.taus[chosen_arm] = 1 + (1 + self.alfa)**self.kas[chosen_arm]
+        self.kas[chosen_arm] += 1
+        
